9b7e302c8f9f28a74bd52ff795d8946d40602d07
Alexis Daniel <86176019+dan-alcubilla@users.noreply.github.com>
2024-01-10 23:01:14 -0400

    added customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,36 @@
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
 import { Metadata } from "next";
+import CustomersTable from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <div>
+                <CustomersTable customers={customers} />
+            </div>
+        </div>
+    );
 }
